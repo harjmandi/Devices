f413ae9e320f55e45a06a3910c20aa88e1e45585
adnsmora <adnsanz@gmail.com>
2020-01-22 16:30:15 +0100

    first Commit

---
+++ Resistance-vs-Temp-fiexGate_USING_HF2LI-Entropy-B2901A.py
@@ -0,0 +1,220 @@
+''' This program uses HF2LI lock-in amplifier to measure the resistance of the sample while cooling. 
+Optionally a constant gate Voltage can be applied using the KEYSIGHT B2901A. 
+The program in the current version is suitable for cooling; small adjustmets are required to use it for warming up.
+
+
+To avoid unneccesary data collection, different measurement intervals (set by measure_pattern_dt) are used at different temperature ranges (set by measure_pattern_T).
+At each measurement time, the program measures the temperature and only if the delta T is large enough (set by measure_pattern_dT) it measures and records the resistance. 
+
+
+
+Hardware to be used:
+	- HF2LI: to measure the resistance of graphene device
+	- A bias resistance of 1M: As voltage to current converter for lock-in out put.
+		Note that there is always errors in reading the resitance of the device; the error is around -33% depending on the gain on S4c (see the excel file "Calibrate S4c gain.xlsx").
+
+	- B2901A: For gating (Optional)
+
+'''
+
+import numpy as np
+import zhinst.utils
+
+import time
+from my_poll import R_measure as R_measure
+import stlab
+import stlabutils
+import os
+import matplotlib.pyplot as plt
+import pygame, sys
+from pygame.locals import *
+import math
+from stlab.devices.He7Temperature import He7Temperature
+
+
+#############################################################
+''' Definitions'''
+
+# IO
+prefix = 'F17_RvsT_'
+sample_name = 'h56'
+do_plot = True
+save_data =True
+
+
+# Gate settings 
+gate = 0 # Choose 0 not to apply any gate
+time_step = 0.1 #time step between each gate voltage steps, to stablize the gate
+ramp_speed = 1500 # the safe speed for ramping the gate voltage [mV/s]
+
+# Temperature settings
+measure_pattern_T = np.array([300, 150, 50, 10, 4]) # Different temperature ranges to set different measurement temperature steps
+
+measure_pattern_dT = np.array([30, 10, 5, 1, 0.1]) # Temperature steps to run a resistance measurements
+# measure_pattern_dT = np.array([0.1, 0.1, 1, 1, 0.1]) # Temperature steps to run a resistance measurements
+
+measure_pattern_dt = np.array([5*60, 2*60, 30, 10, 1]) # Time span between two subsequent temperature readings
+# measure_pattern_dt = np.array([10, 10, 60, 10, 1]) # Time span between two subsequent temperature readings
+
+# HF2LI settings
+bias_resistor = 1e6
+
+
+##########################################################
+''' Initializing the devices '''
+
+# initial configuration of the Lock-in
+apilevel_example = 6  # The API level supported by this example.
+(daq, device, props) = zhinst.utils.create_api_session('dev352', apilevel_example, required_devtype='.*LI|.*IA|.*IS')
+zhinst.utils.api_server_version_check(daq)
+zhinst.utils.disable_everything(daq, device)
+out_mixer_channel = zhinst.utils.default_output_mixer_channel(props)
+
+measure_amplitude = 0.1 #measurement amplitude [V]
+measure_output_channnel = 1
+measure_input_channnel = 1
+measure_frequency = 77 #[Hz]
+demodulation_time_constant = 0.01
+deamodulation_duration = 0.1
+calibration_factor = 1.45 # to compensate the shift in resistance measurement
+
+
+## Temperature readout
+tempdev = He7Temperature(addr='145.94.39.138',verb=False)
+
+## Applying gate
+leakage_current = 0
+
+if gate != 0:
+	from stlab.devices.Keysight_B2901A import Keysight_B2901A
+	B2901A = Keysight_B2901A('TCPIP::192.168.1.63::INSTR')
+	B2901A.SetModeVoltage()
+	B2901A.SetOutputOn()
+	B2901A.SetComplianceCurrent(safe_gate_current)
+	B2901A.RampVoltage(gate,tt=gate/0.5, steps = 20)
+	leakage_current = float(B2901A.GetCurrent())
+
+# IO settings
+pygame.init()
+pygame.display.set_mode((100,100))
+
+#############################################################
+''' MEASUREMENT'''
+
+if save_data:
+	colnames = ['time (s)','temperature (K)', 'gate voltage (V)','leakage current (A)','resistance (ohm)', 'phase ()', 'demodulation duration (s)']
+	my_file= stlab.newfile(prefix+'_',sample_name,autoindex=True,colnames=colnames)
+
+
+
+END = False
+INI_time = time.time()
+t0 = INI_time
+T0 = 300
+
+TIME = np.empty(shape=(0))
+TEMPERATURE = np.empty(shape=(0))
+RESISTANCE = np.empty(shape=(0))
+
+
+while (not END):
+
+	t =  time.time()-INI_time
+	T = tempdev.GetTemperature()
+	print ('Temp = ', T)
+
+	ind = np.where(measure_pattern_T >= T)
+
+
+	if np.abs(T0 - T) > measure_pattern_dT[ind[0][-1]]:
+		print ('MEASUREING ...')
+
+		measured = R_measure(device_id = 'dev352', 
+			amplitude=measure_amplitude,
+			out_channel = measure_output_channnel,
+			in_channel = measure_input_channnel,
+			time_constant = demodulation_time_constant,
+			frequency = measure_frequency,
+			poll_length = deamodulation_duration,
+			device=device, daq=daq,
+			out_mixer_channel=out_mixer_channel,
+			bias_resistor=bias_resistor)
+
+		measured[0]*=(np.cos(math.radians(measured[1]))*calibration_factor*1000) 
+
+		line = [t,T, gate, leakage_current] + measured
+
+		if save_data:
+			stlab.writeline(my_file,line)
+
+		TEMPERATURE = np.append(TEMPERATURE,T)
+		RESISTANCE = np.append(RESISTANCE,measured[0])
+		# TIME = np.append(TIME,t)
+
+		plt.rcParams["figure.figsize"] = [16,9]
+
+
+		plt.subplot(2, 1, 1)
+		plt.plot(TEMPERATURE,RESISTANCE, '--b',marker='.',markersize = 1.5, linewidth=0.5, alpha=0.9)
+
+		plt.ylabel('Resistance ($\Omega$)')
+		plt.xlim(np.min(TEMPERATURE),np.max(TEMPERATURE))
+		plt.title(prefix+ sample_name)
+
+
+		plt.subplot(2, 1, 2)
+		# plt.plot(TIME/60,TEMPERATURE, '--b', marker='.',markersize = 1.5, linewidth=0.5, alpha=0.9)
+		# plt.ylabel('Temperature (K)')
+		# plt.yscale ('log')
+		# plt.xscale ('log')
+		# plt.xlabel('Time (min)')
+
+		plt.plot(TEMPERATURE[:-1], np.diff(RESISTANCE)/np.diff(TEMPERATURE), '--b', marker='.',markersize = 1.5, linewidth=0.5, alpha=0.9)
+		plt.xlabel('Temperature (K)')
+		plt.xlim(np.min(TEMPERATURE),np.max(TEMPERATURE))
+
+		plt.ylabel('$\Delta$R/$\Delta$T ($\Omega/K$)')
+
+		plt.pause(0.1)
+		T0 = T
+
+	else: 
+		print('Elapsed time: {:.0f} min'.format((time.time()-t0)/60))
+		print('Waitng the temperature to fall below {:.2f} ...'.format(T0 - measure_pattern_dT[ind[0][-1]]))
+		print('Measuring every {:.0f}s'.format(measure_pattern_dt[ind[0][-1]]))
+
+
+	
+	while (time.time()-t0 < measure_pattern_dt[ind[0][-1]]) and (not END):
+
+		for event in pygame.event.get():
+			if event.type == QUIT: sys.exit()
+			elif event.type == KEYDOWN and event.dict['key'] == 101:
+				END = True
+				print ('END command detected ...')
+
+		
+
+	t0 = time.time()
+
+
+print('MEASUREMENT FINISHED')
+
+zhinst.utils.disable_everything(daq, device)
+
+if gate !=0:
+	B2901A.RampVoltage(0,tt=ramp_time) # to safely return back the gate voltage
+
+
+#######################################################################
+''' saving the data '''
+
+
+if save_data:
+
+	# saving the metafile
+	plt.savefig(os.path.dirname(my_file.name)+'\\'+prefix)
+	my_file.close()
+
+
+
---
+++ my_poll.py
@@ -0,0 +1,124 @@
+
+from __future__ import print_function
+import time
+import numpy as np
+import zhinst.utils
+
+
+def R_measure(device_id, amplitude, out_channel, in_channel, time_constant, frequency, poll_length, device, daq, out_mixer_channel, bias_resistor):
+    """Run the example: Connect to the device specified by device_id and obtain
+    demodulator data using ziDAQServer's blocking (synchronous) poll() command.
+
+    Returns:
+      sample (dict of numpy arrays): The demodulator sample dictionary with the
+        additional demod R and demod phi fields calculated in the example.
+
+    Raises:
+      RuntimeError: If the device is not "discoverable" from the API.
+    """
+    
+    ''' Initial definitions'''
+  
+
+
+    demod_index = 0
+    osc_index = 0
+    demod_rate = 1e3
+    in_channel-=1
+    out_channel-=1
+
+
+
+
+    exp_setting = [['/%s/sigins/%d/ac'             % (device, in_channel), 0],
+                   ['/%s/sigins/%d/range'          % (device, in_channel), 5*amplitude],
+                   ['/%s/sigins/%d/diff'          % (device, in_channel), 1],
+
+                   ['/%s/demods/%d/enable'         % (device, demod_index), 1],
+                   ['/%s/demods/%d/rate'           % (device, demod_index), demod_rate],
+                   ['/%s/demods/%d/adcselect'      % (device, demod_index), in_channel],
+                   ['/%s/demods/%d/order'          % (device, demod_index), 4],
+                   ['/%s/demods/%d/timeconstant'   % (device, demod_index), time_constant],
+                   ['/%s/demods/%d/oscselect'      % (device, demod_index), osc_index],
+                   ['/%s/demods/%d/harmonic'       % (device, demod_index), 1],
+
+                   ['/%s/oscs/%d/freq'             % (device, osc_index), frequency],
+
+                   ['/%s/sigouts/%d/on'            % (device, out_channel), 1],
+                   ['/%s/sigouts/%d/enables/%d'    % (device, out_channel, out_mixer_channel), 1],
+                   ['/%s/sigouts/%d/range'         % (device, out_channel), 5*amplitude],
+                   ['/%s/sigouts/%d/amplitudes/%d' % (device, out_channel, out_mixer_channel), amplitude]]
+    daq.set(exp_setting)
+
+    # Unsubscribe any streaming data.
+    daq.unsubscribe('*')
+
+    # Wait for the demodulator filter to settle.
+    time.sleep(10*time_constant)
+
+    # Perform a global synchronisation between the device and the data server:
+    # Ensure that 1. the settings have taken effect on the device before issuing
+    # the poll() command and 2. clear the API's data buffers. Note: the sync()
+    # must be issued after waiting for the demodulator filter to settle above.
+    daq.sync()
+
+    # Subscribe to the demodulator's sample node path.
+    path = '/%s/demods/%d/sample' % (device, demod_index)
+    daq.subscribe(path)
+
+    # Sleep for demonstration purposes: Allow data to accumulate in the data
+    # server's buffers for one second: poll() will not only return the data
+    # accumulated during the specified poll_length, but also for data
+    # accumulated since the subscribe() or the previous poll.
+    #sleep_length = 1.0
+    
+    # For demonstration only: We could, for example, be processing the data
+    # returned from a previous poll().
+    #time.sleep(sleep_length)
+
+    # Poll the subscribed data from the data server. Poll will block and record
+    # for poll_length seconds.
+    
+    poll_timeout = 500  # [ms]
+    poll_flags = 0
+    poll_return_flat_dict = True
+    data = daq.poll(poll_length, poll_timeout, poll_flags, poll_return_flat_dict)
+
+    # Unsubscribe from all paths.
+    daq.unsubscribe('*')
+
+    # Check the dictionary returned is non-empty
+    assert data, "poll() returned an empty data dictionary, did you subscribe to any paths?"
+
+    # The data returned is a dictionary of dictionaries that reflects the node's path.
+    # Note, the data could be empty if no data had arrived, e.g., if the demods
+    # were disabled or had demodulator rate 0.
+    assert path in data, "The data dictionary returned by poll has no key `%s`." % path
+
+    # Access the demodulator sample using the node's path.
+    sample = data[path]
+
+    # Let's check how many seconds of demodulator data were returned by poll.
+    # First, get the sampling rate of the device's ADCs, the device clockbase...
+    clockbase = float(daq.getInt('/%s/clockbase' % device))
+    # ... and use it to convert sample timestamp ticks to seconds:
+    dt_seconds = (sample['timestamp'][-1] - sample['timestamp'][0])/clockbase
+    print("poll() returned {:.3f} seconds of demodulator data.".format(dt_seconds))
+    
+    tol_percent = 50
+    
+    assert (dt_seconds - poll_length)/poll_length*100 < tol_percent, \
+        "Duration of demod data returned by poll() (%.3f s) differs " % dt_seconds + \
+        "from the expected duration (%.3f s) by more than %0.2f %%." % \
+        (poll_length, tol_percent)
+
+
+
+    measured_R = np.mean(np.abs(sample['x'] + 1j*sample['y']))*bias_resistor/(amplitude*1e3)
+    measured_phi = np.mean(np.angle(sample['x'] + 1j*sample['y']))
+    measured_duration = dt_seconds
+    
+    #measured = {'resistance':measured_R, 'angle':measured_phi, 'duration':measured_duration}
+    measured = [measured_R, measured_phi, measured_duration]
+
+    return measured
